e038810313554e023af5f46ac7db25fdfacfa9ca
Suley <suleyliu@gmail.com>
2020-05-14 20:48:55 -0500

    Modified the map, center the map, add distance in the features and sort it

--- app.js
+++ app.js
@@ -6,9 +6,10 @@
 
 const formEle = document.querySelector(`form`);
 const inputEle = document.querySelector(`form input`);
-const key = `pk.eyJ1Ijoic3VsYXlsaXUiLCJhIjoiY2thNWlrYmNnMDBpaDNsbm9lOHQ2MG5ncSJ9.iLbn-Tba_v8DH2S_ffwwDA`;
 const ulEle = document.querySelector(`.points-of-interest`);
-const location = [];
+const mapDiv = document.getElementById('map');
+let localLng = 0;
+let localLat = 0;
 
 formEle.addEventListener(`submit`, function(event){
   if(inputEle.value !== ``) {
@@ -18,80 +19,31 @@ formEle.addEventListener(`submit`, function(event){
   event.preventDefault();
 });
 
+navigator.geolocation.getCurrentPosition(success);
+
+mapboxgl.accessToken = `pk.eyJ1Ijoic3VsYXlsaXUiLCJhIjoiY2thNWlrYmNnMDBpaDNsbm9lOHQ2MG5ncSJ9.iLbn-Tba_v8DH2S_ffwwDA`;
+
+let map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v9',
+  center: [-97, 49],
+  zoom: 12
+});
+
+let marker = new mapboxgl.Marker()
+  .setLngLat([-97, 49])
+  .addTo(map);
+
 function success(pos) {
   const crd = pos.coords;
-
-  console.log('Your current position is:');
-  console.log(`Latitude : ${crd.latitude.toFixed(2)}`);
-  console.log(`Longitude: ${crd.longitude.toFixed(2)}`);
-  console.log(`More or less ${crd.accuracy} meters.`);
-
-  location.push(crd.longitude.toFixed(3))
-  location.push(crd.latitude.toFixed(3));
-  getMap(location)
-
+  localLng = crd.longitude;
+  localLat = crd.latitude;
+  map.setCenter([localLng, localLat]);
+  marker.setLngLat([localLng, localLat]);
 }
-
-function error(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-}
-
-navigator.geolocation.getCurrentPosition(success, error);
-
-mapboxgl.accessToken = key;
-
-
-function getMap(array) {
-
-  let map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    center: array,
-    zoom: 12
-  });
-
-  let marker = new mapboxgl.Marker()
-    .setLngLat(array)
-    .addTo(map);
-
-  map.on('load', function() {
-      map.loadImage(
-          'https://upload.wikimedia.org/wikipedia/commons/7/7c/201408_cat.png',
-          function(error, image) {
-              if (error) throw error;
-              map.addImage('cat', image);
-              map.addSource('point', {
-                  'type': 'geojson',
-                  'data': {
-                      'type': 'FeatureCollection',
-                      'features': [
-                          {
-                              'type': 'Feature',
-                              'geometry': {
-                                  'type': 'Point',
-                                  'coordinates': array
-                              }
-                          }
-                      ]
-                  }
-              });
-              map.addLayer({
-                  'id': 'points',
-                  'type': 'symbol',
-                  'source': 'point',
-                  'layout': {
-                      'icon-image': 'cat',
-                      'icon-size': 0.25
-                  }
-              });
-          }
-      );
-  });
-}
-
 
 function searchStreets(name) {
-  fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${name}.json?access_token=${key}`)
+  fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${name}.json?proximity=${localLng},${localLat}&access_token=${mapboxgl.accessToken}&limit=10`)
     .then((resp) => {
       if (resp.ok) {
         return resp.json();
@@ -100,8 +52,8 @@ function searchStreets(name) {
       }
     })
     .then((json) => {
+      // console.log(json.features)
       getStreetList(json.features);
-
     })
 
 }
@@ -109,13 +61,20 @@ function searchStreets(name) {
 function getStreetList(features) {
   let streetHTML = ``;
   features.forEach(feature => {
+    feature.distance = GetDistance(feature.center[0], feature.center[1], localLng, localLat);
+    console.log(feature.distance)
+  });
+  features.sort((a, b) => a.distance - b.distance)
+
+  
+  features.forEach(feature => {
     const name = feature.place_name.split(`,`);
     streetHTML +=  `
       <li class="poi" data-long="${feature.center[0]}" data-lat="${feature.center[1]}">
         <ul>
           <li class="name">${name[0]}</li>
           <li class="street-address">${name[2]} ${name[1]}</li>
-          <li class="distance">5.8 KM</li>
+          <li class="distance">${feature.distance} KM</li>
         </ul>
       </li>`
   });
@@ -123,17 +82,52 @@ function getStreetList(features) {
   ulEle.innerHTML = streetHTML;
 }
 
-// 方法定义 lat,lng 
-function GetDistance( lat1,  lng1,  lat2,  lng2){
+
+function GetDistance(lng1, lat1,  lng2, lat2) {
   var radLat1 = lat1*Math.PI / 180.0;
   var radLat2 = lat2*Math.PI / 180.0;
   var a = radLat1 - radLat2;
   var  b = lng1*Math.PI / 180.0 - lng2*Math.PI / 180.0;
   var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a/2),2) +
   Math.cos(radLat1)*Math.cos(radLat2)*Math.pow(Math.sin(b/2),2)));
-  s = s *6378.137 ;// EARTH_RADIUS;
+  s = s * 6378.137 ;// EARTH_RADIUS;
   s = Math.round(s * 10000) / 10000;
-  return s;
+  return s.toFixed(1);
 }
-// 调用 return的距离单位为km
-GetDistance(10.0,113.0,12.0,114.0)
+
+//if (mapDiv.style.visibility === true) map.resize();
+
+
+// map.on('load', function() {
+//   map.loadImage(
+//       'https://upload.wikimedia.org/wikipedia/commons/7/7c/201408_cat.png',
+//       function(error, image) {
+//           if (error) throw error;
+//           map.addImage('cat', image);
+//           map.addSource('point', {
+//               'type': 'geojson',
+//               'data': {
+//                   'type': 'FeatureCollection',
+//                   'features': [
+//                       {
+//                           'type': 'Feature',
+//                           'geometry': {
+//                               'type': 'Point',
+//                               'coordinates': array
+//                           }
+//                       }
+//                   ]
+//               }
+//           });
+//           map.addLayer({
+//               'id': 'points',
+//               'type': 'symbol',
+//               'source': 'point',
+//               'layout': {
+//                   'icon-image': 'cat',
+//                   'icon-size': 0.25
+//               }
+//           });
+//       }
+//   );
+// });
