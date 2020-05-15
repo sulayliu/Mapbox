c8f9b6e7389090fe0503c12b330a7a98b5504fd7
Suley <suleyliu@gmail.com>
2020-05-14 21:24:46 -0500

    Set flyto the location

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-// const options = {
-//   enableHighAccuracy: true,
-//   timeout: 5000,
-//   maximumAge: 0
-// };
-
 const formEle = document.querySelector(`form`);
 const inputEle = document.querySelector(`form input`);
 const ulEle = document.querySelector(`.points-of-interest`);
-const mapDiv = document.getElementById('map');
+const mapDiv = document.querySelector('#map');
+const links = document.querySelector(`.points-of-interest`);
 let localLng = 0;
 let localLat = 0;
 
@@ -18,6 +13,15 @@ formEle.addEventListener(`submit`, function(event){
   inputEle.value = ``;
   event.preventDefault();
 });
+
+links.addEventListener(`click`, function(event) {
+  if (event.target.closest(`li`).className = `poi`) {
+    const lng = event.target.closest(`aside > ul > li`).dataset.long;
+    const lat = event.target.closest(`aside > ul > li`).dataset.lat;
+    map.flyTo({center: [lng, lat], essential: true});
+    marker.setLngLat([lng, lat]);
+  }
+})
 
 navigator.geolocation.getCurrentPosition(success);
 
@@ -52,7 +56,6 @@ function searchStreets(name) {
       }
     })
     .then((json) => {
-      // console.log(json.features)
       getStreetList(json.features);
     })
 
@@ -60,10 +63,11 @@ function searchStreets(name) {
 
 function getStreetList(features) {
   let streetHTML = ``;
+
   features.forEach(feature => {
     feature.distance = GetDistance(feature.center[0], feature.center[1], localLng, localLat);
-    console.log(feature.distance)
   });
+
   features.sort((a, b) => a.distance - b.distance)
 
   
@@ -84,12 +88,12 @@ function getStreetList(features) {
 
 
 function GetDistance(lng1, lat1,  lng2, lat2) {
-  var radLat1 = lat1*Math.PI / 180.0;
-  var radLat2 = lat2*Math.PI / 180.0;
+  var radLat1 = lat1 * Math.PI / 180.0;
+  var radLat2 = lat2 * Math.PI / 180.0;
   var a = radLat1 - radLat2;
-  var  b = lng1*Math.PI / 180.0 - lng2*Math.PI / 180.0;
-  var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a/2),2) +
-  Math.cos(radLat1)*Math.cos(radLat2)*Math.pow(Math.sin(b/2),2)));
+  var  b = lng1 * Math.PI / 180.0 - lng2 * Math.PI / 180.0;
+  var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) +
+  Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
   s = s * 6378.137 ;// EARTH_RADIUS;
   s = Math.round(s * 10000) / 10000;
   return s.toFixed(1);
